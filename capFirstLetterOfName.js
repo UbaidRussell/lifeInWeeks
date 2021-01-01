f7ac89a73837c664ab65f6ee5b8ee4be5b5e831a
UbaidRussell <UbaidRussell@gmail.com>
2021-01-01 04:08:11 -0500

    Create capFirstLetterOfName.js

---
+++ capFirstLetterOfName.js
@@ -0,0 +1,27 @@
+//step 1 
+//we need a variable to store the name a user enters into the prompt
+
+var name = prompt("What is yourName?");
+
+//step 2
+//we need to be able to capitalize the first letter of their name
+
+//We can isolate this step
+//a isolate the first char
+
+var firstChar = name.slice(0,1);
+
+
+//b turn the first char to uppercase
+var upperCaseFirstChar = firstChar.toUpperCase();
+
+//c isolate the rest of the name  
+var restOfName = name.slice(1,name.length);
+
+//d concatenate the first char with the rest of the char
+var wholeName = upperCaseFirstChar + restOfName; 
+
+//Step 3 
+//We use the capitalized version in order to greet them using a alert 
+
+alert("Hello, " + wholeName);
