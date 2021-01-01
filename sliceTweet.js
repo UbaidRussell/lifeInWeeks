9d9f7e71635cbd7d3cc0b3a883c7860e38e2e980
UbaidRussell <UbaidRussell@gmail.com>
2021-01-01 02:58:37 -0500

    Update sliceTweet.js

--- sliceTweet.js
+++ sliceTweet.js
@@ -1,4 +1,9 @@
+//first way
 var tweet = prompt("Compose your tweet:");
 var tweetUnder140 = tweet.slice(0,140);
 
 alert(tweetUnder140);
+
+
+// second way
+alert(prompt("Compose your tweet: ").slice(0,140));
