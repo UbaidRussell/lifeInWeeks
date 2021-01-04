815dc27c5d2bc80c451c7aaad96448b55e73846c
UbaidRussell <UbaidRussell@gmail.com>
2021-01-03 23:22:22 -0500

    Update loveScore.js

--- loveScore.js
+++ loveScore.js
@@ -4,5 +4,11 @@ var name2 = prompt("Give me another name");
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
+if (loveScore > 70){
+    alert("Your love score is " + loveScore + "%" + " and you love each other like kanye loves kanye");
+}else {
+    alert("Your love score is " + loveScore + "%");
+}
+
 
 console.log(name1 + " and "+ name2 + " have a love rate of " + loveScore +"%");
