34c5ea4cd5a5699482137e4f9ab27cfc53071afe
UbaidRussell <UbaidRussell@gmail.com>
2021-01-03 21:59:20 -0500

    Create loveScore.js

---
+++ loveScore.js
@@ -0,0 +1,8 @@
+var name1 = prompt("Give me a name");
+var name2 = prompt("Give me another name");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+
+console.log(name1 + " and "+ name2 + " have a love rate of " + loveScore);
