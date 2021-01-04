f0c78bb83c2eff15ec4504a1592f11c28d86e6fd
UbaidRussell <UbaidRussell@gmail.com>
2021-01-03 21:49:18 -0500

    Create randomNumber.js

---
+++ randomNumber.js
@@ -0,0 +1,11 @@
+//Get random number and places it in n
+var n = Math.random();
+
+//Creates a range of numbers between 0.01 and 9
+n = n * 10;
+
+//rounds the number 
+n = Math.floor(n) + 1;
+
+//Console log 
+console.log(n);
