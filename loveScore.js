5f08f975d3892907c7836593788df1a5488ba036
UbaidRussell <UbaidRussell@gmail.com>
2021-01-05 17:57:49 -0500

    Update loveScore.js

--- loveScore.js
+++ loveScore.js
@@ -6,9 +6,17 @@ loveScore = Math.floor(loveScore) + 1;
 
 if (loveScore > 70){
     alert("Your love score is " + loveScore + "%" + " and you love each other like kanye loves kanye");
-}else {
+}
+if (loveScore > 30 && loveScore <= 70) { 
+    alert("Your love score is " + loveScore + "%");
+}
+if (loveScore <= 30) { 
+    alert("Your love score is " + loveScore + "%" + "you go together like oil and water");
+}
+else {
     alert("Your love score is " + loveScore + "%");
 }
 
 
 console.log(name1 + " and "+ name2 + " have a love rate of " + loveScore +"%");
+
