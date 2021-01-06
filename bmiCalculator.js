2d066ee3f44587365d3ec9873421b6cfe2f89cff
UbaidRussell <UbaidRussell@gmail.com>
2021-01-05 19:23:40 -0500

    Update bmiCalculator.js

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,7 +1,14 @@
 function bmiCalculator (weight, height) {
     var bmi = weight / Math.pow(height, 2);
-    return Math.round(bmi);
+    var interpretation = "Your BMI is " + bmi;
+    
+    if (bmi < 18.5){
+        interpretation = interpretation + ", so you are underweight.";
+    } else if (bmi >= 18.5 && bmi <= 24.9){
+        interpretation = interpretation + ", so you have a normal weight.";
+    } else {
+        interpretation = interpretation + ", so you are overweight.";
+    }
+    
+    return interpretation;
 }
-
-var bmi = bmiCalculator(65, 1.8)
-console.log(bmi);
