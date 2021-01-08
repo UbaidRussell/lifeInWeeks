eb2d2c59d7d1960837185e7c2f9c64e1012ea614
UbaidRussell <UbaidRussell@gmail.com>
2021-01-08 08:35:38 -0500

    +while loop

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,30 +3,22 @@ var count = 1;
 
 function fizzBuzz(){
 
-//outputs fizzbuzz if count is divisible by both 3 and 5
-if (count % 3 === 0 && count % 5 === 0){
-    output.push("FizzBuzz");
+while (count <= 100){        
+    if (count % 3 === 0 && count % 5 === 0){
+        output.push("FizzBuzz");
+    }
+    else if (count % 3 === 0){
+        output.push("Fizz");
+    }
+    else if(count % 5 === 0 ){
+        output.push("Buzz");
+    }
+    else{
+        output.push(count);
+    }
+    //Increments count by 1
+    count++;
 }
-
-//outputs fizz if count is divisible by 3
-else if (count % 3 === 0){
-    output.push("Fizz");
-}
-
-//outputs buzz if count is divisible by 5
-else if(count % 5 === 0 ){
-    output.push("Buzz");
-}
-//outputs count
-else{
-    output.push(count);
-}
-
-
-
-//Increments count by 1
-count++;
-
  //just logs the output
     console.log(output);
 }
