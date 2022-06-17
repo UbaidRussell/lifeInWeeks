5c6254a6cee96eabefd39a717aa32b0c2fa49551
Russell <UbaidRussell@gmail.com>
2022-06-17 18:04:41 -0400

    Update numOfMonitiors.js

--- numOfMonitiors.js
+++ numOfMonitiors.js
@@ -1,7 +1,10 @@
-function monitorCount(rows, columns){
- return rows * columns; 
+const totalCost = costOfMonitors(5, 4);
+
+function monitorCount(rows, columns) {
+  return rows * columns;
 }
 
-
-const numOfMonitors = monitorCount(5,4);
-console.log(numOfMonitors);
+function costOfMonitors(rows, columns){
+return monitorCount(rows, columns) * 200;
+}
+console.log(totalCost);
