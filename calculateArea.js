67ca56a3214353bdc59390413f748ff728c025a4
Russell <UbaidRussell@gmail.com>
2022-06-17 20:16:54 -0400

    Create calculateArea.js

---
+++ calculateArea.js
@@ -0,0 +1,5 @@
+//Messing around. The goal is to calculate area using a single line function block. 
+const calculateArea = (height, width) => height * width;
+
+console.log(calculateArea(10, 10));
+
