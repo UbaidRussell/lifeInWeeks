4cff76551dc41d5e377e78cab4ad39ef9de5287b
Ubaid <UbaidRussell@gmail.com>
2022-08-18 19:31:13 -0400

    Create isEven.js

---
+++ isEven.js
@@ -0,0 +1,13 @@
+//Checks if a number is even or Odd
+
+function isEven(num){
+  if(num % 2 == 0){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+
+console.log(isEven(/*Enter Number here*/));
