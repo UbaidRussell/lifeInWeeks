bbe1a3724bb5c37703e327832267e10a1e8cdf02
Ubaid <UbaidRussell@gmail.com>
2022-08-19 19:40:14 -0400

    Create numberDigits.js

---
+++ numberDigits.js
@@ -0,0 +1,10 @@
+function numberDigits(x){
+  if (x >= 0 && x <= 9){
+    return `One digit: ${x}`
+  }else if (x >=10 && x <= 99){
+    return `Two digits: ${x}`
+  } else {
+    return `The number is: ${x}`
+  }
+
+}
