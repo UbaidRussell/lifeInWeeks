3b45be90411e67ed995cbe35fb8b1ff9cb38074e
Ubaid <UbaidRussell@gmail.com>
2022-08-24 05:20:13 -0400

    Update accessingElementsInArrays.js

--- accessingElementsInArrays.js
+++ accessingElementsInArrays.js
@@ -1,1 +1,12 @@
+//Here is our Array
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
 
+//Her we declarae a variable and set it to index's in our array
+let listItem = famousSayings[0];
+console.log(listItem);
+
+//This one outputs where there is love there is life. 
+console.log(famousSayings[2]);
+
+//This one outputs undefined since we begin our array at 0 and there is no 3rd element in our array. 
+console.log(famousSayings[3]);
