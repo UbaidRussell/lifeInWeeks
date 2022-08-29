e8873f93f54f3bb592d77611e65c033ee29a9696
Ubaid <UbaidRussell@gmail.com>
2022-08-28 20:48:52 -0400

    Update dogAge.js

--- dogAge.js
+++ dogAge.js
@@ -1,3 +1,5 @@
+//This program gets us dog age in human years and is a great program to use if you want to figure out your dogs age in human years.
+
 var dogAge = prompt("What is your dogs age?");
 var humanAge = (dogAge - 2) * 4 + 21;
 
