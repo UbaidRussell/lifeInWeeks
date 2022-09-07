b3efc1afd09b50b33ba94f4814599d2aeb486a19
Ubaid <UbaidRussell@gmail.com>
2022-09-07 17:23:30 -0400

    Update guestList.js

--- guestList.js
+++ guestList.js
@@ -5,6 +5,8 @@ var guestName = prompt("What is your name?");
 
 if (guestList.includes(guestName)){
     alert("Welcome!");
+    //Pushes the Guest name into the guestList array
+    guestList.push(guestName);
 } else {
     alert("Sorry, maybe next time.");
 }
