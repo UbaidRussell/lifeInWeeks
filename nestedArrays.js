241e3da850d2d3add05966bdf716f95be1cd829d
U <UbaidRussell@gmail.com>
2022-09-13 05:13:43 -0400

    Create nestedArrays.js

---
+++ nestedArrays.js
@@ -0,0 +1,7 @@
+//Here is an example of a nested array 
+const numberClusters = [[1,2],[3,4],[5,6]];
+
+//Here we use a variable to teach us how to target a element inside of a nested array. Our goal here is to target 6
+const target = numberClusters[2][1];
+//Checking our code results to make sure we're right.
+console.log(target)
