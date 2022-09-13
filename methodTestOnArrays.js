e81e55f2aaaf4842d4e6470f5aa1d5febb90d3f0
U <UbaidRussell@gmail.com>
2022-09-12 20:32:55 -0400

    Comments

--- methodTestOnArrays.js
+++ methodTestOnArrays.js
@@ -1,11 +1,11 @@
-const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains']; //This is our Array
 
-groceryList.shift();
-groceryList.unshift('popcorn');
-console.log(groceryList);
+groceryList.shift(); //Here we remove the first element 'Orange Juice' from our array.
+groceryList.unshift('popcorn');//here we add popcorn to the first element in our array.
+console.log(groceryList); // Logs our array. 
 
-console.log(groceryList.slice(1, 4));
+console.log(groceryList.slice(1, 4)); //Here we use the slice method to slice off array items 1 - 4
 console.log(groceryList)
 
-const pastaIndex = groceryList.indexOf('pasta');
+const pastaIndex = groceryList.indexOf('pasta'); //Here we created a variable to check what index is the pasta element. 
 console.log(pastaIndex)
