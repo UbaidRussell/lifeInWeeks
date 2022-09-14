925936ae12c968e3c9e95aa270322214c2183bda
U <UbaidRussell@gmail.com>
2022-09-14 19:21:43 -0400

    Update isEven.js

--- isEven.js
+++ isEven.js
@@ -1,4 +1,4 @@
-//Checks if a number is even or Odd
+//This function checks if a number is even or not by using an if staement and seeing if num divided by 2 equals 0
 
 function isEven(num){
   if(num % 2 == 0){
