75a9962fec065eb40c35d03917b6c44bde965552
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-02-17 14:56:02 -0500

    Added comments 🔧

--- class.js
+++ class.js
@@ -1,3 +1,4 @@
+//Here we created a function that allows us to create student objects with different properties
 const studentCreation = (name, age, gender, quote) => {
     return{
         name: name, 
@@ -9,10 +10,11 @@ const studentCreation = (name, age, gender, quote) => {
     }
 }
 
+//Here we use that function and create those objects of students giving them different properties
 const jefferey = studentCreation('Jefferey Whitlock', 20, 'male', 'You got this')
 const victoria = studentCreation('Victoria Samson', 21, 'Female', 'We will take the lead this year')
 
-
+//We simply call our objects to the console here. 
 console.log(jefferey)
 console.log(victoria)
 console.log(jefferey.yearQuote());
