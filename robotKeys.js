0375691372c340af2b506b0ccdc78b29af5c3dce
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-02-18 21:55:52 -0500

    added robotKeys.js

---
+++ robotKeys.js
@@ -0,0 +1,14 @@
+//This code is us testing the Built in Object method Object.keys() on a robot method we created
+
+const robot = {
+    model: 'SAL-1000',
+    mobile: true,
+    sentient: false,
+    armor: 'Steel-plated',
+    energyLevel: 75
+};
+
+const robotKeys = Object.keys(robot); //Assigning a variable is one way we can get our out.
+console.log(robotKeys)
+
+console.log(Object.keys(robot))//This is another way we can also get the same output. 
