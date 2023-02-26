96021414d4d6989af7a0ce874fa4360ce64ee00b
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-02-26 18:37:36 -0500

    Updated the playground code 🔭

--- playgroundJavascript.js
+++ playgroundJavascript.js
@@ -9,6 +9,10 @@ console.log('This is the output on announceThatIamDoingImportantWork:')
 console.log(busy());
 
 
+
+
+
+
 //Second javascript program playground code
 const higherOrderFunc = param => {
     param();
@@ -24,6 +28,9 @@ console.log('This is the output for the higherOrder function:')
 console.log(higherOrderFunc(anotherFunc));
 
 
+
+
+
 //Third javascript program playground code 
 const addTwo = num => {
     return num + 2;
@@ -37,3 +44,30 @@ const addTwo = num => {
   
   console.log('This is the output of checkConsistentOuput:')
   console.log(checkConsistentOutput(addTwo, 1));
+
+
+
+
+  //fourth javascript playground code
+  const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+  console.log('This is the output for fruits.forEach method and a version where we use a arrow function:')
+
+  // Iterate over fruits below
+  fruits.forEach(function(fruit){
+    console.log(`I want to eat a ${fruit}`)
+  });
+
+  //This code creates a space
+  console.log(' ')
+  
+  
+  //Here's we use a arrow function
+  fruits.forEach(fruits => console.log(`I want to eat a ${fruits}`))
+  
+  
+  //This is a arrow function we created to mess around with arrow functions
+  const sayMyName = name => {
+     console.log(name)
+  }
+  
+  sayMyName('U')
