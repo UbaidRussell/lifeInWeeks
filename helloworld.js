dc6906d70672874fc26541a66f2f5244378f8e21
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-03-28 21:59:04 -0400

    Tested out our Ai

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,20 @@
-console.log('Hello world. This program is a test');
+function helloWorld() {
+    console.log('Hello world. This program is a test');
+    console.log('This is a test of the emergency broadcast system');
+    console.log('If this had been an actual emergency, you would have been instructed to do something');
+    console.log('This is only a test to see if our ai is alive and working with us, no sex please');
+}
+
+function calculateBooks(book) {
+    return book * 200;
+}
+
+function whatHappenedHere(){
+    console.log('We wrote a function that calculates the number of books we have in our library and we wrote a function that prints out a message to the console');
+    console.log('We did this to test our ai and see if it is working with us')
+    console.log('Good job ai 🥂')
+}
+
+helloWorld();
+console.log(calculateBooks(2));
+whatHappenedHere();
--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -51,4 +51,3 @@ function playGame(){
 }
   
 playGame();
-  
