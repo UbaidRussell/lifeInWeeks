80bd897820569dc2b3c7184c318d2c7c9ad53450
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-11-10 12:59:54 -0500

    Got our code to work in the browser ⚡️

--- slicetweet/sliceTweet.js
+++ slicetweet/sliceTweet.js
@@ -1,3 +1,5 @@
+let setTweet = document.querySelector('h1');
+
 //Below are 2 ways to slice a string when you only want it to contain a certain number of characters.
 
 
@@ -5,7 +7,9 @@
 var tweet = prompt("Compose your tweet:");
 var tweetUnder140 = tweet.slice(0,140);
 
-alert(tweetUnder140);
+setTweet.innerHTML = tweetUnder140;
 
-// second way
-alert(prompt("Compose your tweet: ").slice(0,140));
+//alert(tweetUnder140);
+
+// second way We're not going to use this way here
+//alert(prompt("Compose your tweet: ").slice(0,140));
