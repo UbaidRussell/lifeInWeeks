435e13772300e89515f4c1b5a06d4b4f61520a53
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-11-11 17:15:59 -0500

    Created calculateArea folder and moved javascript into it 🔧

---
+++ calculateArea/calculateArea.js
@@ -0,0 +1,8 @@
+//Messing around. The goal is to calculate area using a single line function block. 
+const calculateArea = (height, width) => height * width;
+
+let height = prompt("Choose your height")
+let width = prompt("Choose your Width:")
+
+console.log(calculateArea(10, 10));
+
