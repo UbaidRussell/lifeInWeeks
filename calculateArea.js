af42cbd72fd21d42063349b3264db14902b08121
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-11-12 15:25:35 -0500

    Moved Javascript file 📦

--- calculateArea.js
+++
@@ -1,5 +0,0 @@
-//Messing around. The goal is to calculate area using a single line function block. 
-const calculateArea = (height, width) => height * width;
-
-console.log(calculateArea(10, 10));
-
