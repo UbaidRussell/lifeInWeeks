f0a380a102204d6225e9da08a21d0b187f01672b
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-12-10 23:31:09 -0500

    Got user input 🤍

--- toEmoticon/toEmoticon.js
+++ toEmoticon/toEmoticon.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('myForm');
-//An emoticon is a group of keyboard characters like [ : ) ] that typically represent emoji's but are instead made only with a keyboard and a dab of creativity. Here's we tinkered with a few and developed a program that can output one depending on the input.
 
+
+
+//An emoticon is a group of keyboard characters like [ : ) ] that typically represent emoji's but are instead made only with a keyboard and a dab of creativity. Here's we tinkered with a few and developed a program that can output one depending on the input.
 const toEmoticon = emoticon => {
   switch (emoticon){
     case 'shrug':
@@ -22,29 +24,14 @@ const toEmoticon = emoticon => {
   }
 }
 
-/*
-// script.js
-function getValue() {
-  // Access the form and select element
-  var form = document.getElementById('myForm');
-  var dropdown = form.elements['emojiChoices'];
-
-  // Get the selected value
-  var selectedValue = dropdown.value;
-
-  // Use the value as needed
-  console.log(selectedValue);
-}*/
 
 const getValue = () => {
   // Access the form and select element
-  var dropdown = form.elements['emojiChoices'];
+  var dropdown = document.getElementById('emojiChoices');
 
   // Get the selected value
   var selectedValue = dropdown.value;
-  var selectedValueText = dropdown.options[dropdown.selectedIndex].text;
 
   // Use the value as needed
-  console.log(selectedValue);
-  console.log(selectedValueText);
+  return selectedValue;
 }
