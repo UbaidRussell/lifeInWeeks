22a110a604d63d42330a8951101623d561b0c12c
UbaidRussell <UbaidRussell@users.noreply.github.com>
2023-12-15 15:57:05 -0500

    Got console to log the input we entered ❤️‍🔥

--- isEven/isEven.js
+++ isEven/isEven.js
@@ -1,5 +1,6 @@
+const form = document.querySelector('form');
+let userInput;
 //This function checks if a number is even or not by using an if staement and seeing if num divided by 2 equals 0
-
 function isEven(num){
   if(num % 2 == 0){
     return true
@@ -8,6 +9,27 @@ function isEven(num){
     return false
   }
 }
+
+//This gets our userInput and logs it to the console
+function getUserInput(){
+  // Access the input element
+  var userInputElement = document.getElementById('number');
+
+  // Get the value entered by the user
+  var userInputValue = userInputElement.value;
+
+  // Use the value as needed
+  console.log('User entered:', userInputValue);
+}
+
+
+
+//This stops our page from reloading
+document.addEventListener('DOMContentLoaded', function() {
+  form.addEventListener('submit', function (event){
+    event.preventDefault();
+  });
+});
 
 
 console.log(isEven(/*Enter Number here*/));
